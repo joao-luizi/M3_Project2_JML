7471c9fb839f9c830780956c9e465625b1022b30
llm-Lucas <lucaslemosm@hotmail.com>
2026-04-14 01:42:09 +0100

    Models and Repositories Added

---
+++ XPTOBusiness/Repositories/INucleoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XPTOBusiness.Models;
+
+namespace XPTOBusiness.Repositories
+{
+    public interface INucleoRepository
+    {
+        void GetAll();
+        void GetById(int id);
+        void Add(Nucleo nucleo);
+        void Update(Nucleo nucleo);
+        void Delete(int id);
+    }
+}
---
+++ XPTOBusiness/Repositories/NucleoRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XPTOBusiness.Models;
+
+namespace XPTOBusiness.Repositories
+{
+    public class NucleoRepository : INucleoRepository
+    {
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Nucleo nucleo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Nucleo nucleo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
