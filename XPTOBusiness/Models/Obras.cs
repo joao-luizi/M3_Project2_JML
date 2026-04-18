c5f45180364eedf26d29bae7fe88e6b87c3f6168
Luis <maialuis87@gmail.com>
2026-04-18 16:40:33 +0100

    Added Models: Obras, Exemplares, Assuntos

---
+++ XPTOBusiness/Models/Obras.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPTOBusiness.Models
+{
+    public class Obra
+    {
+        public long ID_Obra { get; set; }
+        public string? Autor { get; set; }
+        public string? Titulo { get; set; } = string.Empty;
+        public byte[]? Capa { get; set; } = null; // mudar para VARBINARY(MAX) no SSMS
+        public string? ISBN { get; set; }
+        public byte ID_Assunto { get; set; }
+
+    }
+}
