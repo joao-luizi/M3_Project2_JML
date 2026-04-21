360dfe60531a90a7fdf0f3a0c66b86f6503cd5f2
llm-Lucas <lucaslemosm@hotmail.com>
2026-04-21 14:21:10 +0100

    MOD correção da interface ITipoUtilizadoresRepositories, que estava em branco

--- XPTOBusiness/Repositories/ITipoUtilizadoresRepository.cs
+++ XPTOBusiness/Repositories/ITipoUtilizadoresRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XPTOBusiness.Models;
 
 namespace XPTOBusiness.Repositories
 {
-    internal class ITipoUtilizadoresRepositories
+    public interface ITipoUtilizadoresRepositories
     {
+        List<TipoUtilizador> GetAll(string tag);
+        TipoUtilizador GetById(int id, string tag);
+        int Insert(TipoUtilizador tu, string tag);
+        void Update(TipoUtilizador tu, string tag);
+        void Delete(int id, string tag);
     }
 }
